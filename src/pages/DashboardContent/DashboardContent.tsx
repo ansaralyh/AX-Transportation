0c11e1fdf0abc7f21a9a91afc41e9f696ff041be
Ansar Ali <ansaralyh@gmail.com>
2025-02-27 15:49:49 +0500

    Dashoboard layout ready

--- src/pages/DashboardContent/DashboardContent.tsx
+++ src/pages/DashboardContent/DashboardContent.tsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const DashboardContent = () => {
   return (
-    <div>DashboardContent</div>
+    <div className='mt-4 ml-6'>
+      <div className='border-3 flex g'>
+      <div className='border-2 border-red-500 bg-[#FFFFFF] w-[600px] h-[842px]'></div>
+      <div className='-500 w-full'></div>
+      </div>
+      <h2>Upcoming Assignments</h2>
+      <div className='border-2 border-green-500 w-full h-[340px]'>
+
+      </div>
+      <div className='border-2 border-green-500 w-full h-[340px] mt-20'>
+
+      </div>
+    </div>
   )
 }
 
