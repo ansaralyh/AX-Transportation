a0b7e23f3d40f962c81c9b4e22cec458de3e236b
Ansar Ali <ansaralyh@gmail.com>
2025-02-27 17:29:43 +0500

    Dashoboard layout ready

--- src/pages/DashboardContent/DashboardContent.tsx
+++ src/pages/DashboardContent/DashboardContent.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
 
 const DashboardContent = () => {
   return (
     <div className='mt-4 ml-6'>
       <div className='border-3 flex g'>
-      <div className='border-2 border-red-500 bg-[#FFFFFF] w-[600px] h-[842px]'></div>
-      <div className='-500 w-full'></div>
+      <div className='rounded bg-[#FFFFFF] shadow-2xl w-[600px] h-[842px] px-4'>
+        <h1 className='text-[#092C4C] text-3xl font-bold mb-6'>Drivers</h1>
+        <input
+      type="text"
+      placeholder="Q Search Driver"
+      className="w-full h-[44px] p-2 border-none bg-[#F4F6F9]  rounded-lg focus:outline-none focus:ring-2 focus:ring-[#F4F6F9]"
+    />
+      </div>
+      <div className=' w-full'></div>
       </div>
       <h2>Upcoming Assignments</h2>
       <div className='border-2 border-green-500 w-full h-[340px]'>
