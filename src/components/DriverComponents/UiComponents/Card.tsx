20f15210d82d3dbead56cb61ba24500db04a64cc
Ansar Ali <ansaralyh@gmail.com>
2025-03-01 01:08:53 +0500

    Driver Cards added

--- src/components/DriverComponents/UiComponents/Card.tsx
+++ src/components/DriverComponents/UiComponents/Card.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import { FaUser, FaCar, FaStar, FaPhone, FaMapMarkerAlt } from "react-icons/fa";
 
 const Card = () => {
   return (
-    <div>Card</div>
-  )
-}
+    <div className="w-[412px] h-[182px] bg-[#EFEFEF] rounded-lg mt-6 flex justify-center gap-8 p-4">
+      <div className="flex flex-col gap-5">
+        <img src="/public/assets/Avatar.png" alt="" />
+        <FaStar className="text-yellow-500" />
+        <span className="text-sm text-gray-600">4.5/2,256</span>
+      </div>
+      <div className="flex flex-col gap-[8px]">
+        <div className="flex items-center gap-2">
+          <FaUser className="text-gray-600" />
+          <span className="text-lg font-semibold">Michael Nguyen</span>
+        </div>
+        <div className="flex items-center gap-2">
+          <FaCar className="text-gray-600" />
+          <span className="text-sm text-gray-600">
+            Pontiac - White - 586 5GX
+          </span>
+        </div>
+        <div className="flex items-center gap-2">
+          <FaPhone className="text-gray-600" />
+          <span className="text-sm text-gray-600">0903001276</span>
+        </div>
+        <div className="flex items-center gap-2">
+          <FaMapMarkerAlt className="text-gray-600" />
+          <span className="text-sm text-gray-600">Saigon</span>
+        </div>
+        <div className="bg-[#95FEA5] rounded-lg w-[190px] text-[14px] px-[12px] py-1 h-9 text-[#2E374C] font-semibold">
+          On the way to destination
+        </div>
+      </div>
+      {/* icons */}
+      <div className="flex gap-1">
+        <img
+          src="/public/assets/Icon Button (4).png"
+          alt=""
+          className="h-6 w-6"
+        />
+        <img
+          src="/public/assets/Icon Button (5).png"
+          alt=""
+          className="h-6 w-6"
+        />
+      </div>
+     
+    </div>
+  );
+};
 
-export default Card
+export default Card;
--- src/pages/DashboardContent/DashboardContent.tsx
+++ src/pages/DashboardContent/DashboardContent.tsx
@@ -1,28 +1,38 @@
+import Card from "../../components/DriverComponents/UiComponents/Card";
 
 const DashboardContent = () => {
   return (
-    <div className='mt-4 ml-6'>
-      <div className='border-3 flex g'>
-      <div className='rounded bg-[#FFFFFF] shadow-2xl w-[600px] h-[842px] px-4'>
-        <h1 className='text-[#092C4C] text-3xl font-bold mb-6'>Drivers</h1>
-        <input
-      type="text"
-      placeholder="Q Search Driver"
-      className="w-full h-[44px] p-2 border-none bg-[#F4F6F9]  rounded-lg focus:outline-none focus:ring-2 focus:ring-[#F4F6F9]"
-    />
-
-      </div>
-      <div className=' w-full'></div>
+    <div className="mt-4 ml-6">
+      <div className="border-3 flex g">
+        <div className="rounded bg-[#FFFFFF] shadow-2xl w-[600px] h-[842px] px-4">
+          <h1 className="text-[#092C4C] text-3xl font-bold mb-6">Drivers</h1>
+          <input
+            type="text"
+            placeholder="Q Search Driver"
+            className="w-full h-[44px] p-2 border-none bg-[#F4F6F9]  rounded-lg focus:outline-none focus:ring-2 focus:ring-[#F4F6F9]"
+          />
+          <div className="">
+            <Card />
+            <Card />
+            <Card />
+          </div>
+          {/* Buttons */}
+          <div className="flex  gap-2 mt-4">
+            <button className="hover:bg-[#FB8A00] w-[184px] font-bold h-[60px] hover:text-white  text-[#000000] px-4 py-2 rounded-lg border-2 hover:border ">
+              See More
+            </button>
+            <button className="hover:bg-[white] hover:text-[#000000] font-bold w-[184px] h-[60px] bg-[#FB8A00] text-[#000000] px-4 py-2 rounded-lg hover:border-2">
+              View Profile
+            </button>
+          </div>
+        </div>
+        <div className=" w-full"></div>
       </div>
       <h2>Upcoming Assignments</h2>
-      <div className='border-2 border-green-500 w-full h-[340px]'>
-
-      </div>
-      <div className='border-2 border-green-500 w-full h-[340px] mt-20'>
-
-      </div>
+      <div className="border-2 border-green-500 w-full h-[340px]"></div>
+      <div className="border-2 border-green-500 w-full h-[340px] mt-20"></div>
     </div>
-  )
-}
+  );
+};
 
-export default DashboardContent
+export default DashboardContent;
