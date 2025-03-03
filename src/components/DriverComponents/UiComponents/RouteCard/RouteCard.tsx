f6345fd682f2798ff57e9e2a1d0ab628459786f6
aliyasmuhammad12 <aliyasmuhammad1122@gmail.com>
2025-03-03 16:37:50 +0500

    done responsivenes

--- src/components/DriverComponents/UiComponents/RouteCard/RouteCard.tsx
+++ src/components/DriverComponents/UiComponents/RouteCard/RouteCard.tsx
@@ -9,12 +9,12 @@ interface RouteCardProps {
 
 export const RouteCard: React.FC<RouteCardProps> = ({ title, subtitle, description, imageUrl }) => {
   return (
-    <div className="bg-white p-4 rounded-xl shadow-md flex items-center justify-between w-full">
+    <div className="bg-white p-4 rounded-xl shadow-md flex flex-wrap gap-10 items-center justify-center md:justify-between w-full">
       <div>
         <p className="font-medium text-[27px]">{title} <span className=" text-[25px]">({subtitle})</span></p>
         <p className=" text-[25px] max-w-[500px] mt-2">{description}</p>
       </div>
-      <img src={imageUrl} alt={title} className=" object-contain" />
+      <img src={imageUrl} alt={title}  className=" object-contain" />
     </div>
   );
 };
